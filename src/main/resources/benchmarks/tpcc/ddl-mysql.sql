38ea3e968707a4af7c873e880213d943803a6855
cosmos <gogo9th@hanmail.net>
2023-06-03 16:05:38 +0900

    TPCC NewOrder stock bug fix

--- src/main/resources/benchmarks/tpcc/ddl-mysql.sql
+++ src/main/resources/benchmarks/tpcc/ddl-mysql.sql
@@ -239,7 +239,7 @@ NewOrder_Label:BEGIN
 
   Order_Loop:WHILE (var_loop_cnt < var_o_ol_cnt) DO
     SET var_i_id = (NonUniformRandom(8191, 7911, 1, var_configItemCount));
-    SET var_ol_quantity = (RandomNumber(1, 10));
+    SET var_ol_quantity = RandomNumber(1, 10);
 
     SELECT I_PRICE INTO var_i_price FROM item WHERE I_ID = var_i_id;
 
@@ -269,8 +269,8 @@ NewOrder_Label:BEGIN
 
     INSERT INTO order_line (OL_O_ID, OL_D_ID, OL_W_ID, OL_NUMBER, OL_I_ID, OL_SUPPLY_W_ID, OL_QUANTITY, OL_AMOUNT, OL_DIST_INFO) VALUES (var_d_next_o_id, var_d_id, var_w_id, var_loop_cnt, var_i_id, var_ol_supply_w_id, var_ol_quantity, var_ol_quantity * var_i_price, var_s_dist_info);
 
-    UPDATE stock SET S_YTD := S_YTD + var_ol_quantity,
-                     S_ORDER_CNT := S_ORDER_CNT + 1,
+    UPDATE stock SET S_YTD := S_YTD + var_s_quantity,
+                     S_ORDER_CNT := S_ORDER_CNT + var_ol_quantity,
                      S_REMOTE_CNT := S_REMOTE_CNT + var_s_remote_cnt_increment
     WHERE S_I_ID = var_i_id AND S_W_ID = var_ol_supply_w_id;
 
